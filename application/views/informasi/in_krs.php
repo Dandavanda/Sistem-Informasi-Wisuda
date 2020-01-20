fdbfa9700a5f32b9690f0f93fab1d801dabb94d2
novanda <dandavanda@gmail.com>
2020-01-21 03:34:54 +0700

    edit view yudisium

--- application/views/informasi/in_krs.php
+++ application/views/informasi/in_krs.php
@@ -7,94 +7,46 @@ require_once('head.php');?>
 <h3>PROSEDUR dan SYARAT CETAK KRS ONLINE
 
 oleh mahasiswa sendiri</h3>
+<br><br>
+
+
 <p>
-Mahasiswa telah menyelesaikan seluruh beban administrasi baik SPP tetap maupun SPP VARIABLE. <br>
+1. Mahasiswa telah menyelesaikan seluruh beban administrasi baik SPP tetap maupun SPP VARIABLE. <br>
+
+
+2. Mahasiswa telah mengisikan KRS Online di amikom.ac.id <br>
  
 
-Mahasiswa telah mengisikan KRS Online di amikom.ac.id <br>
+3. Mahasiswa mencetak sendiri KRS UJIAN melalui Account masing-masing di amikom.ac.id <br>
  
 
-Mahasiswa mencetak sendiri KRS UJIAN melalui Account masing-masing di amikom.ac.id <br>
+4. Mahasiswa menyimpan KRS UJIAN yang telah dicetak sebagai syarat untuk mengikuti Ujian. <br>
  
 
-Mahasiswa menyimpan KRS UJIAN yang telah dicetak sebagai syarat untuk mengikuti Ujian. <br>
+5. Pencetakan KRS UJIAN Akan dibuka secara system sesuai tanggal yang ditentukan kemudian <br>
  
 
-Pencetakan KRS UJIAN Akan dibuka secara system sesuai tanggal yang ditentukan kemudian <br>
+6. Mahasiswa yang tidak bisa mencetak sendiri KRS UJIAN nya secara online di amikom.ac.id dipersilahkan melakukan konfirmasi dan verifikasi data di DPK (Direktorat Perencanaan dan Keuangan) <br>
  
 
-Mahasiswa yang tidak bisa mencetak sendiri KRS UJIAN nya secara online di amikom.ac.id dipersilahkan melakukan konfirmasi dan verifikasi data di DPK (Direktorat Perencanaan dan Keuangan) <br>
- 
-
-Mahasiswa yang belum menyelesaikan beban administrasi, maka tidak bisa melakukan pencetakan sendiri secara online dan dimohon segera konfirmasi di DPK dengan membawa bukti kwitansi pembayaran spp variable semester yang bersangkutan. <br>
- 
-
- 
-
-KRS UJIAN digunakan untuk syarat ujian, apabila KRS UJIAN hilang maka tidak ada penggantian KRS UJIAN sehingga mahasiswa dipersilahkan mencetak kembali KRS UJIAN melalui online system di amikom.ac.id <br>
- 
-
-Mahasiswa diharuskan menyimpan dengan baik berkas KRS UJIAN online yang digunakan untuk ujian tengah dan akhir semester, sebagai bahan kroscek data apabila dikemudian hari bermasalah dengan permasalahan Akademis (baik UTS maupun UAS). <br>
- 
-
-Ukuran Kertas untuk KRS ujian adalah HVS A4 80 gram.
+7. Mahasiswa yang belum menyelesaikan beban administrasi, maka tidak bisa melakukan pencetakan sendiri secara online dan dimohon segera konfirmasi di DPK dengan membawa bukti kwitansi pembayaran spp variable semester yang bersangkutan. <br>
  
 
  
 
+8. KRS UJIAN digunakan untuk syarat ujian, apabila KRS UJIAN hilang maka tidak ada penggantian KRS UJIAN sehingga mahasiswa dipersilahkan mencetak kembali KRS UJIAN melalui online system di amikom.ac.id <br>
+ 
+
+9. Mahasiswa diharuskan menyimpan dengan baik berkas KRS UJIAN online yang digunakan untuk ujian tengah dan akhir semester, sebagai bahan kroscek data apabila dikemudian hari bermasalah dengan permasalahan Akademis (baik UTS maupun UAS) Ukuran Kertas untuk KRS ujian adalah HVS A4 80 gram. <br>
+ 
+<br>
+<br>
  
 
 Yogyakarta, 1 Oktober 2017
-
+<br>
 DAAK
 
 
 
 
-Mahasiswa telah menyelesaikan seluruh beban administrasi baik SPP tetap maupun SPP VARIABLE.
- 
-
-Mahasiswa telah mengisikan KRS Online di amikom.ac.id
- 
-
-Mahasiswa mencetak sendiri KRS UJIAN melalui Account masing-masing di amikom.ac.id
- 
-
-Mahasiswa menyimpan KRS UJIAN yang telah dicetak sebagai syarat untuk mengikuti Ujian.
- 
-
-Pencetakan KRS UJIAN Akan dibuka secara system sesuai tanggal yang ditentukan kemudian
- 
-
-Mahasiswa yang tidak bisa mencetak sendiri KRS UJIAN nya secara online di amikom.ac.id dipersilahkan melakukan konfirmasi dan verifikasi data di DPK (Direktorat Perencanaan dan Keuangan)
- 
-
-Mahasiswa yang belum menyelesaikan beban administrasi, maka tidak bisa melakukan pencetakan sendiri secara online dan dimohon segera konfirmasi di DPK dengan membawa bukti kwitansi pembayaran spp variable semester yang bersangkutan.
- 
-
- 
-
-KRS UJIAN digunakan untuk syarat ujian, apabila KRS UJIAN hilang maka tidak ada penggantian KRS UJIAN sehingga mahasiswa dipersilahkan mencetak kembali KRS UJIAN melalui online system di amikom.ac.id
- 
-
-Mahasiswa diharuskan menyimpan dengan baik berkas KRS UJIAN online yang digunakan untuk ujian tengah dan akhir semester, sebagai bahan kroscek data apabila dikemudian hari bermasalah dengan permasalahan Akademis (baik UTS maupun UAS).
- 
-
-Ukuran Kertas untuk KRS ujian adalah HVS A4 80 gram.
- 
-
- 
-
- 
-
-Yogyakarta, 1 Oktober 2017
-
-DAAK
-
-</p>
-
-</div>
-<?php 
-
-require_once('footer.php'); ?>
- 
